e7badabc5ed64ba8d5960dbbf3800474cdb82276
Sebastien Tourneux <sebastientourneux1@gmail.com>
2025-04-10 00:43:28 +0200

    [Stitch] ADD FFT phase correlation method

--- cli/parse_arguments.py
+++ cli/parse_arguments.py
@@ -45,6 +45,7 @@ def add_subparser_process(subparsers):
 
 def add_subparser_stitch(subparsers):
 	parser_stitch = subparsers.add_parser('stitch', help='Stitch pictures together')
+	parser_stitch.add_argument("--method", help="Stitching method", dest="method", choices=["feature", "fourier"], default="feature", type=str)
 	parser_stitch.add_argument("--match_ratio_threshold", help="Threshold of match quality", dest="match_ratio_threshold", default=0.5, type=float)
 
 def parse_rgb(value):
